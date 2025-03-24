acb1db4fb21db491016900e0494cca6ce74fa489
Fluky294 <fooksr.21@gmail.com>
2025-03-24 14:30:36 +0700

    add api Update attractions

--- app/api/attractions/route.js
+++ app/api/attractions/route.js
@@ -10,19 +10,35 @@ export async function GET(request) {
     )
     return NextResponse.json(rows, { status: 200 })
   } catch (error) {
-    return NextResponse.json({ error: "Failed to fetch" }, { status: 500 })
+    return NextResponse.json({ error: error }, { status: 500 })
   }
 }
 
-export async function POST(request){
+export async function POST(request) {
   try {
-      const {name, detail,coverimage, latitude, longitude} = await request.json();
-      const [result] = await db.query(
-          'INSERT INTO attractions (name, detail,coverimage, latitude, longitude) VALUES (?,?,?,?,?)',[name, detail,coverimage, latitude, longitude]
-      );
-      return NextResponse.json({id:result.insertId,name, detail,coverimage, latitude, longitude}, {status: 200});
-  } catch (error){
-      return NextResponse.json({error:"Failed to create"}, {status: 500})
+    const { name, detail, coverimage, latitude, longitude } = await request.json();
+    const [result] = await db.query(
+      'INSERT INTO attractions (name, detail,coverimage, latitude, longitude) VALUES (?,?,?,?,?)', [name, detail, coverimage, latitude, longitude]
+    );
+    return NextResponse.json({ id: result.insertId, name, detail, coverimage, latitude, longitude }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 })
   }
 }
+
+export async function PUT(request) {
+  try {
+    const { id, name, detail, coverimage, latitude, longitude } = await request.json();
+    const [result] = await db.query(
+      'UPDATE attractions SET name=?, detail=?, coverimage=?, latitude=?, longitude=? WHERE id = ?', [name, detail, coverimage, latitude, longitude, id]
+    );
+    if (result.affectedRows == 0) {
+      return NextResponse.json({ error: "ID Not found" }, { status: 404 });
+    }
+    return NextResponse.json({ message: "Updated", id }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 })
+  }
+}
+
 
